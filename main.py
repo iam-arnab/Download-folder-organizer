664e16f1657bfdaee6a03be0964ee113e6315463
Arnab <arnab@pop-os.localdomain>
2020-09-29 15:33:28 +0530

    Removed redundancy of same code with function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import shutil
 import os
 filetype = {
     'video': ['.mp4', '.mkv', '.3gp', '.avi', '.vob', '.m2ts', '.mov', '.mpg4'],
-    'music': ['.mp3', '.3ga', '.aifc', '.m3u', '.m3u8', '.m4p', '.m4r'],
+    'music': ['.mp3', '.3ga', '.aifc', '.m3u', '.m3u8', '.m4p', '.m4r', '.opus'],
     'image': ['.jpg', '.png', '.xcf', '.jpeg', '.gif', '.tif', '.webp'],
     'docs': ['.json', '.pdf', '.txt', '.doc', '.html', '.htm', '.xls', '.xlsx', '.ppt', '.pptx', '.docx', '.csv', '.dat', '.db', '.dbf', '.log', '.mdb', '.sav', '.sql', '.xml', '.epub'],
     'compress': ['.gz', '.xz', '.bz', '.zip', '.rar', '.7z', '.arj'],
@@ -17,112 +17,28 @@ filetype = {
 path = '/home/arnab/Downloads'
 files = os.listdir(path)
 
-for file in files:
-    temp = file.split('.')
-    ext = '.' + temp[-1]
-    for i in filetype['video']:
-        if i == ext:
-            if os.path.isdir(os.path.join(path, 'Video')):
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, 'Video'))
-            else:
-                os.mkdir(os.path.join(path, 'Video'))
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, 'Video'))
-for file in files:
-    temp = file.split('.')
-    ext = '.' + temp[-1]
-    for i in filetype['music']:
-        if i == ext:
-            if os.path.isdir(os.path.join(path, 'Music')):
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, 'Music'))
-            else:
-                os.mkdir(os.path.join(path, 'Music'))
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, 'Music'))
-for file in files:
-    temp = file.split('.')
-    ext = '.' + temp[-1]
-    for i in filetype['image']:
-        if i == ext:
-            if os.path.isdir(os.path.join(path, 'Images')):
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, 'Images'))
-            else:
-                os.mkdir(os.path.join(path, 'Images'))
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, 'Images'))
-for file in files:
-    temp = file.split('.')
-    ext = '.' + temp[-1]
-    for i in filetype['docs']:
-        if i == ext:
-            if os.path.isdir(os.path.join(path, 'Documents')):
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, 'Documents'))
-            else:
-                os.mkdir(os.path.join(path, 'Documents'))
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, 'Documents'))
-for file in files:
-    temp = file.split('.')
-    ext = '.' + temp[-1]
-    for i in filetype['compress']:
-        if i == ext:
-            if os.path.isdir(os.path.join(path, 'Compressed')):
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, 'Compressed'))
-            else:
-                os.mkdir(os.path.join(path, 'Compressed'))
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, 'Compressed'))
-for file in files:
-    temp = file.split('.')
-    ext = '.' + temp[-1]
-    for i in filetype['disk']:
-        if i == ext:
-            if os.path.isdir(os.path.join(path, 'Disk Images')):
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, "'Disk Images'"))
-            else:
-                os.mkdir(os.path.join(path, "Disk Images"))
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, "Disk Images"))
-for file in files:
-    temp = file.split('.')
-    ext = '.' + temp[-1]
-    for i in filetype['programs']:
-        if i == ext:
-            if os.path.isdir(os.path.join(path, 'Programs')):
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, 'Programs'))
-            else:
-                os.mkdir(os.path.join(path, 'Programs'))
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, 'Programs'))
-for file in files:
-    temp = file.split('.')
-    ext = '.' + temp[-1]
-    for i in filetype['torrent']:
-        if i == ext:
-            if os.path.isdir(os.path.join(path, 'Torrents')):
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, 'Torrents'))
-            else:
-                os.mkdir(os.path.join(path, 'Torrents'))
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, 'Torrents'))
 
-for file in files:
-    temp = file.split('.')
-    ext = '.' + temp[-1]
-    for i in filetype['fonts']:
-        if i == ext:
-            if os.path.isdir(os.path.join(path, 'Fonts')):
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, 'Fonts'))
-            else:
-                os.mkdir(os.path.join(path, 'Fonts'))
-                shutil.move(os.path.join(path, file),
-                            os.path.join(path, 'Fonts'))
+def organize(file_type, file_folder):
+    for file in files:
+        temp = file.split('.')
+        ext = '.' + temp[-1]
+        for i in filetype[file_type]:
+            if i == ext:
+                if os.path.isdir(os.path.join(path, file_folder)):
+                    shutil.move(os.path.join(path, file),
+                                os.path.join(path, file_folder))
+                else:
+                    os.mkdir(os.path.join(path, file_folder))
+                    shutil.move(os.path.join(path, file),
+                                os.path.join(path, file_folder))
+
+
+organize('video', 'Video')
+organize('music', 'Music')
+organize('image', 'Images')
+organize('docs', 'Documents')
+organize('compress', 'Compressed')
+organize('disk', 'Disk Images')
+organize('torrent', 'Torrents')
+organize('fonts', 'Fonts')
+organize('programs', 'Programs')
